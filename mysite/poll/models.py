ddb353d503f184ab74c3272ba2551d3c32df8ac2
Bachirou06 <djibo.bachirou06@gmail.com>
2025-08-30 12:51:12 +0200

    Play around the model

--- mysite/poll/models.py
+++ mysite/poll/models.py
@@ -3,12 +3,15 @@ from datetime import timedelta
 from django.db import models
 from django.utils import timezone
 # Create your models here.
+# My django project
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
     def __str__(self):
         return f"{self.question_text}"
+
+    import pdb; pdb.set_trace()
     @property
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
